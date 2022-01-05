a4dbb936f59f552246e5f90f54304dbe5e17fec7
david_venegas <venegasdavidm@gmail.com>
2022-01-05 13:56:48 -0500

    income kinda styled

--- client/src/2-Main/Budget/Budget.js
+++ client/src/2-Main/Budget/Budget.js
@@ -3,8 +3,59 @@ import React from 'react'
 
 function Budget() {
     return (
-        <div>
-            <h1>BUDGET</h1>
+        <div className='budget-container'>
+            <div className="budget-header">
+                <div className="budget-header-box1">
+                    <h1 className='main-section-title'>Monthly Budget</h1>
+                    <p className='main-section-description'>Follow your Expenses</p>
+                </div>
+                <div className="budget-header-box2">
+                    <h2 className='budget-category-title'>Categories</h2>
+                    <div className="budget-category-item">--------------</div>
+                </div>
+                <div className="budget-header-box3">
+                    <h2 className='budget-outline-title'>This Month:</h2>
+                    <h4 className="budget-outline-item">Savings: <span>$4,000</span></h4>
+                    <h4 className="budget-outline-item">Expenses: <span>$2,000</span></h4>
+                    <h4 className="budget-outline-item">Total: <span>$2,000</span></h4>
+                </div>
+            </div>
+            <div className="budget-body">
+                <table>
+                    <tr>
+                        <th>Category</th>
+                        <th>Amount Spent</th>
+                    </tr>
+                    <tr>
+                        <td>Food</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Housing</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Transportation</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Personal Care</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Education</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Entertainment</td>
+                        <td>$0</td>
+                    </tr>
+                    <tr>
+                        <td>Other</td>
+                        <td>$0</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     )
 }
--- client/src/2-Main/Income/Income.js
+++ client/src/2-Main/Income/Income.js
@@ -11,23 +11,23 @@ function Income() {
                 </div>
                 <div className="income-header-box2">
                     <h2 className='income-category-title'>Savings</h2>
-                    <div className="income-category-amount"><span>$</span><h2>12,000</h2></div>
+                    <div className="income-category-amount"><h2><span>$</span> 12,000</h2></div>
                 </div>
                 <div className="income-header-box3">
                     <h2 className='income-category-title'>Expenses</h2>
-                    <div className="income-category-amount"><span>$</span><h2>2,000</h2></div>
+                    <div className="income-category-amount"><h2><span>$</span> 2,000</h2></div>
                 </div>
                 <div className="income-header-box4">
                     <h2 className='income-category-title'>Total Balance</h2>
-                    <div className="income-category-amount"><span>$</span><h2>10,000</h2></div>
+                    <div className="income-category-amount"><h2><span>$</span> 10,000</h2></div>
                 </div>
             </div>
             <div className="income-form-container">
                 <form className="income-form">
-                    <input type="text" />
-                    <input type="number" />
-                    <select className="income-category">
-                        <option disabled selected hidden>---</option>
+                    <input className='income-form-header' type="text" placeholder='Description...' />
+                    <input className='income-form-header' type="number" placeholder='Amount...' />
+                    <select className="income-form-header">
+                        <option disabled selected hidden>Category...</option>
                         <option value="food">Food</option>
                         <option value="housing">Housing</option>
                         <option value="transportation">Transportation</option>
@@ -37,35 +37,63 @@ function Income() {
                         <option value="savings">Savings</option>
                         <option value="other">Other</option>
                     </select>
-                    <button type="submit">Add Transaction</button>
+                    <button className='income-submit-button' type="submit">Add Transaction</button>
                 </form>
             </div>
             <div className="income-body">
                 <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Date</th>
-                        <th>Amount</th>
-                        <th>Category</th>
-                    </tr>
-                    <tr>
-                        <td>Burger King</td>
-                        <td>01/04/2022</td>
-                        <td>$40</td>
-                        <td>Food</td>
-                    </tr>
-                    <tr>
-                        <td>Lululemon</td>
-                        <td>01/12/2022</td>
-                        <td>$100</td>
-                        <td>Personal Care</td>
-                    </tr>
-                    <tr>
-                        <td>Paycheck</td>
-                        <td>01/14/2022</td>
-                        <td>$2000</td>
-                        <td>Savings</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Date</th>
+                            <th>Amount</th>
+                            <th>Category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Burger King</td>
+                            <td>01/04/2022</td>
+                            <td>$40</td>
+                            <td>Food</td>
+                        </tr>
+                        <tr>
+                            <td>Lululemon</td>
+                            <td>01/12/2022</td>
+                            <td>$100</td>
+                            <td>Personal Care</td>
+                        </tr>
+                        <tr>
+                            <td>Paycheck</td>
+                            <td>01/14/2022</td>
+                            <td>$2000</td>
+                            <td>Savings</td>
+                        </tr>
+                        <tr>
+                            <td>Paycheck</td>
+                            <td>01/14/2022</td>
+                            <td>$2000</td>
+                            <td>Savings</td>
+                        </tr>
+                        <tr>
+                            <td>Paycheck</td>
+                            <td>01/14/2022</td>
+                            <td>$2000</td>
+                            <td>Savings</td>
+                        </tr>
+                        <tr>
+                            <td>Paycheck</td>
+                            <td>01/14/2022</td>
+                            <td>$2000</td>
+                            <td>Savings</td>
+                        </tr>
+                        <tr>
+                            <td>Paycheck</td>
+                            <td>01/14/2022</td>
+                            <td>$2000</td>
+                            <td>Savings</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
