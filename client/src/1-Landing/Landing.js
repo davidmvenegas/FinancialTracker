fb1e4cc10d612f180a3ed6d1eb95d76c56d5dc73
david_venegas <venegasdavidm@gmail.com>
2022-01-06 00:29:59 -0500

    one rule, one system

--- client/src/1-Landing/Landing.js
+++ client/src/1-Landing/Landing.js
@@ -1,22 +1,60 @@
 import './landing.css'
-import { React } from 'react'
+import { React, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useFinanceContext } from '../FinanceContext'
-import Registration from './Registration'
-import Login from './Login'
+import axios from 'axios'
 
 function Landing() {
-    const { loggedInStatus } = useFinanceContext()
+    const { loggedInStatus, handleSuccesfulAuth } = useFinanceContext()
+    const [logEmail, setLogEmail] = useState("")
+    const [logPassword, setLogPassword] = useState("")
+    const [regEmail, setRegEmail] = useState("")
+    const [regPassword, setRegPassword] = useState("")
+    const [passwordConfirmation, setPasswordConfirmation] = useState("")
+
+    const handleLogEmailChange = (e) => setLogEmail(e.target.value)
+    const handleLogPasswordChange = (e) => setLogPassword(e.target.value)
+    const handleRegEmailChange = (e) => setRegEmail(e.target.value)
+    const handleRegPasswordChange = (e) => setRegPassword(e.target.value)
+    const handlePasswordConfirmationChange = (e) => setPasswordConfirmation(e.target.value)
+
+    function handleRegSubmit(e) {
+        e.preventDefault()
+        axios.post('/registrations', {
+            user: {
+                email: regEmail,
+                password: regPassword,
+                passwordConfirmation: passwordConfirmation
+            }
+        }, { withCredentials: true })
+        .then(res => handleSuccesfulAuth(res.data))
+        .catch(err => console.error(err))
+    }
+
+    function handleLogSubmit(e) {
+        e.preventDefault()
+        axios.post('/sessions', {
+            user: {
+                email: logEmail,
+                password: logPassword,
+            }
+        }, { withCredentials: true })
+        .then(res => handleSuccesfulAuth(res.data))
+        .catch(err => console.error(err))
+    }
 
     return (
         <div>
             <h1>LANDING PAGE</h1>
             <p>Status: {loggedInStatus}</p>
-            <h1>REGISTER</h1>
-            <Registration/>
-            <h1>LOGIN</h1>
-            <Login/>
             <Link to='/main'>Go to Main</Link>
+
+            <form onSubmit={handleLogSubmit}></form>
+            <form onSubmit={handleRegSubmit}></form>
+
+
+
+
         </div>
     )
 }
