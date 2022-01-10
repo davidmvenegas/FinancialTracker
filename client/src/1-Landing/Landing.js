e15901dc2d053b67de46be9227b27a2ccce15d0f
david_venegas <venegasdavidm@gmail.com>
2022-01-09 20:07:54 -0500

    email reset working on it...

--- client/src/1-Landing/Landing.js
+++ client/src/1-Landing/Landing.js
@@ -2,9 +2,10 @@ import './landing.css'
 import { React, useState } from 'react'
 import { useFinanceContext } from '../FinanceContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import swal from 'sweetalert';
 import Logo from '../images/logo.png'
-import axios from 'axios'
+import Particles from "react-tsparticles";
 
 function Landing() {
     const navigate = useNavigate()
@@ -15,6 +16,10 @@ function Landing() {
     const [username, setUsername] = useState("")
     const [regPassword, setRegPassword] = useState("")
     const [passwordConfirmation, setPasswordConfirmation] = useState("")
+    const [boxSlide, setBoxSlide] = useState('')
+    const [logSlide, setLogSlide] = useState('')
+    const [regSlide, setRegSlide] = useState('form-hidden')
+    const [passSlide, setPassSlide] = useState('form-hidden')
 
     const handleLogEmailChange = (e) => setLogEmail(e.target.value)
     const handleLogPasswordChange = (e) => setLogPassword(e.target.value)
@@ -23,14 +28,27 @@ function Landing() {
     const handleRegPasswordChange = (e) => setRegPassword(e.target.value)
     const handlePasswordConfirmationChange = (e) => setPasswordConfirmation(e.target.value)
 
-    const loginBtn = document.querySelectorAll(".login-btn")
-    const registerBtn = document.querySelectorAll(".register-btn")
-    const lostPassBtn = document.querySelectorAll(".lost-pass-btn")
-    const box = document.querySelector(".box")
-    const loginForm = document.querySelector(".login-form")
-    const registerForm = document.querySelector(".register-form")
-    const lostPasswordForm = document.querySelector(".lost-password-form")
-
+    function clickedRegisterBtn() {
+        setBoxSlide('slide-active')
+        setLogSlide('form-hidden')
+        setPassSlide('form-hidden')
+        setRegSlide('')
+        clearLogForm()
+    }
+    function clickedLoginBtn() {
+        setRegSlide('form-hidden')
+        setPassSlide('form-hidden')
+        setBoxSlide('')
+        setLogSlide('')
+        clearRegForm()
+    }
+    function clickedResetPassword() {
+        setRegSlide('form-hidden')
+        setLogSlide('form-hidden')
+        setPassSlide('')
+        clearLogForm()
+        clearRegForm()
+    }
     function clearRegForm() {
         setRegEmail('')
         setUsername('')
@@ -41,34 +59,6 @@ function Landing() {
         setLogEmail('')
         setLogPassword('')
     }
-
-    registerBtn.forEach((btn) =>{
-        btn.addEventListener("click", () => {
-            box.classList.add("slide-active")
-            registerForm.classList.remove("form-hidden")
-            loginForm.classList.add("form-hidden")
-            lostPasswordForm.classList.add("form-hidden")
-            clearLogForm()
-        })
-    })
-    loginBtn.forEach((btn) =>{
-        btn.addEventListener("click", () => {
-            box.classList.remove("slide-active")
-            registerForm.classList.add("form-hidden")
-            loginForm.classList.remove("form-hidden")
-            lostPasswordForm.classList.add("form-hidden")
-            clearRegForm()
-        })
-    })
-    lostPassBtn.forEach((btn) =>{
-        btn.addEventListener("click", () => {
-            registerForm.classList.add("form-hidden")
-            loginForm.classList.add("form-hidden")
-            lostPasswordForm.classList.remove("form-hidden")
-            clearLogForm()
-            clearRegForm()
-        })
-    })
 
     function handleRegSubmit(e) {
         e.preventDefault()
@@ -116,25 +106,38 @@ function Landing() {
 
     return (
         <div>
+        <Particles options={{
+            interactivity: {
+                events: {onClick: {enable: true, mode: "push"},
+                onHover: {enable: true, mode: "repulse"}},
+                modes: {bubble: {distance: 800, duration: 2, opacity: 1, size: 500},
+                push: {quantity: 4}, repulse: {distance: 100, duration: 1}
+            }},
+            particles: {
+                color: {value: ["#666"]},
+                links: {color: "#111", distance: 200, enable: true, opacity: 1, width: .125},
+                move: {direction: "none", enable: true, outMode: "bounce", random: true, speed: 2.5, straight: false},
+                number: {value: 75},
+                opacity: {value: 1}
+            }}}/>
             <div className="login-page">
                 <div className="login-logo-box">
                     <div className="login-content-box">
                         <img src={Logo} alt="Logo" />
                         <h2>The Financial Tracker <span>&reg;</span></h2>
                     </div>
-                    <p>Monitor your spending - Understand your finances - Prepare for your future.</p>
                 </div>
-                <div className="box">
+                <div className={`box ${boxSlide}`}>
                     <div className="left">
                         <h3>Create Account</h3>
-                        <button type="button" className="register-btn">Register</button>
+                        <button type="button" onClick={clickedRegisterBtn} className="register-btn">Register</button>
                     </div>
                     <div className="right">
                         <h3>Have an Account ?</h3>
-                        <button type="button" className="login-btn">Login</button>
+                        <button type="button" className="login-btn" onClick={clickedLoginBtn} >Login</button>
                     </div>
                     <div className="form">
-                        <form onSubmit={handleLogSubmit} className="login-form">
+                        <form onSubmit={handleLogSubmit} className={`login-form ${logSlide}`}>
                             <h3>Log In</h3>
                             <div className="form-group">
                                 <input onChange={handleLogEmailChange} value={logEmail} type="text" placeholder="Email Address" className="form-control" required />
@@ -145,9 +148,9 @@ function Landing() {
                                 <span className="landing-input-focus"></span>
                             </div>
                             <button type="submit" className="submit-btn">Login</button>
-                            <p className="lost-pass-btn">Lost Your Password ?</p>
+                            <p onClick={clickedResetPassword} className="lost-pass-btn">Lost Your Password ?</p>
                         </form>
-                        <form onSubmit={handleRegSubmit} className="register-form form-hidden">
+                        <form onSubmit={handleRegSubmit} className={`register-form ${regSlide}`}>
                             <h3>Register</h3>
                             <div className="form-group">
                                 <input onChange={handleUsernameChange} value={username} type="text" placeholder="Name" className="form-control" required />
@@ -166,16 +169,16 @@ function Landing() {
                                 <span className="landing-input-focus"></span>
                             </div>
                             <button type="submit" className="submit-btn">Register</button>
-                            <p className="lost-pass-btn">Lost Your Password ?</p>
+                            <p onClick={clickedResetPassword} className="lost-pass-btn">Lost Your Password ?</p>
                         </form>
-                        <form className="lost-password-form form-hidden">
+                        <form className={`lost-password-form ${passSlide}`}>
                             <h3>Lost Your Password ?</h3>
                             <h5>You will receive a link to create a new password via email.</h5>
                             <div className="form-group">
                                 <input type="text" placeholder="Email Address" className="form-control" required />
                             </div>
                             <button type="submit" className="submit-btn">Reset Password</button>
-                            <p><span className="login-btn">Login</span> | <span className="register-btn">Register</span></p>
+                            <p><span className="login-btn" onClick={clickedLoginBtn} >Login</span> | <span onClick={clickedRegisterBtn} className="register-btn">Register</span></p>
                         </form>
                     </div>
                 </div>
